c87cca25ed80ccbb9e00c684d775131cddafc59c
krisiic-hue <kiwijelinsk@gmail.com>
2022-05-18 22:31:22 +0300

    working on header  and made a component for adding/removing products

---
+++ src/components/AddRemoveOptions.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="addRemoveOptions">
+        <i @click="addProducts">Add</i>
+        <i @click="removeProducts"></i>
+        <i @click="closePage">x</i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddRemoveOptions',
+    methods: {
+        addProducts() {
+            console.log('product added')
+        },
+
+        removeProducts() {
+            console.log('product removed');
+        },
+
+        closePage() {
+            document.getElementById('addRemoveOptions').style.display = "none";
+        }
+    }
+}
+</script>
+
+<style scoped>
+#addRemoveOptions {
+    height: 25px;
+    width: 100px;
+    background-color: beige;
+    color: black;
+    border: 0.5px solid grey;
+    float: right;
+    margin-right: 13%;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,28 @@
 <template >
     <div id="app">
         <h1>{{ title }}</h1>
-        <i id="addRemoveIcon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M352 448H32c-17.69 0-32 14.31-32 32s14.31 31.1 32 31.1h320c17.69 0 32-14.31 32-31.1S369.7 448 352 448zM48 208H160v111.1c0 17.69 14.31 31.1 32 31.1s32-14.31 32-31.1V208h112c17.69 0 32-14.32 32-32.01s-14.31-31.99-32-31.99H224v-112c0-17.69-14.31-32.01-32-32.01S160 14.33 160 32.01v112H48c-17.69 0-32 14.31-32 31.99S30.31 208 48 208z"/></svg></i>
+        <i @click="addProducts" id="addRemoveIcon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M352 448H32c-17.69 0-32 14.31-32 32s14.31 31.1 32 31.1h320c17.69 0 32-14.31 32-31.1S369.7 448 352 448zM48 208H160v111.1c0 17.69 14.31 31.1 32 31.1s32-14.31 32-31.1V208h112c17.69 0 32-14.32 32-32.01s-14.31-31.99-32-31.99H224v-112c0-17.69-14.31-32.01-32-32.01S160 14.33 160 32.01v112H48c-17.69 0-32 14.31-32 31.99S30.31 208 48 208z"/></svg></i>
+    </div>
+    <div v-if="addProducts()" id="options">
+        <AddRemoveOptions />
     </div>
 </template>
 
 <script>
+import AddRemoveOptions from './AddRemoveOptions.vue';
 
 export default {
-    name: 'HeaderMain',
+    name: "HeaderMain",
     props: {
         title: String,
     },
+    components: { 
+        AddRemoveOptions 
+    },
+    methods: {
+        addProducts() {
+        }
+    }
 }
 
 </script>
@@ -45,6 +56,10 @@ h1 {
     width: 20px;
     float: right;
     padding: 10px;
+}
+
+#options {
+    display: none;
 }
 
 </style>
